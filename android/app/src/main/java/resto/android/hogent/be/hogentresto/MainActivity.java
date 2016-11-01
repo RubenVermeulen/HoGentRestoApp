e68f56b4a14550ae1ef45927f39786271d2e7d1c
Jonas Wallays <jonas.wallays.u8978@student.hogent.be>
2016-11-01 21:31:28 +0100

    android basis homescreen

--- android/app/src/main/java/resto/android/hogent/be/hogentresto/MainActivity.java
+++ android/app/src/main/java/resto/android/hogent/be/hogentresto/MainActivity.java
@@ -2,12 +2,20 @@ package resto.android.hogent.be.hogentresto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.titel_homescreen);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
