8e171c3c798ec962bb6c94929dc39dfa527620a9
Alexander Van Damme <alexanderdebest@live.be>
2016-11-16 17:39:34 +0100

    referentie toegevoegd naar menu

--- web/models/Allergenen.js
+++ web/models/Allergenen.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 var AllergeenSchema = new mongoose.Schema({
-  naam: String
+  naam: String,
+  post: {type: mongoose.Schema.Types.ObjectId, ref: 'Menu'}
 });
 
 mongoose.model('Allergeen', AllergeenSchema);
