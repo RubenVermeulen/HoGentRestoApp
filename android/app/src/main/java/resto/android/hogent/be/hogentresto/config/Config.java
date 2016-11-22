073e649f94b5c0828755d83ccfb985a3f0d5acb0
Ruben Vermeulen <ruben.vermeulen@hotmail.com>
2016-11-22 11:46:41 +0100

    Updates URL REST API

--- android/app/src/main/java/resto/android/hogent/be/hogentresto/config/Config.java
+++ android/app/src/main/java/resto/android/hogent/be/hogentresto/config/Config.java
@@ -1,5 +1,5 @@
 package resto.android.hogent.be.hogentresto.config;
 
 public class Config {
-    public static String baseUrl = "http://ec2-54-93-95-178.eu-central-1.compute.amazonaws.com:3000/";
+    public static String baseUrl = "http://azure-rvns2.rubenvermeulen.be/";
 }
