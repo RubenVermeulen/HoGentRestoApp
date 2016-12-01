9eebe662ce63450b38db0bd0828242197139fcad
Jonas Wallays <jonas.wallays.u8978@student.hogent.be>
2016-12-01 16:08:35 +0100

    Android viewpager

--- android/app/src/main/java/resto/android/hogent/be/hogentresto/adapters/MenuAdapter.java
+++ android/app/src/main/java/resto/android/hogent/be/hogentresto/adapters/MenuAdapter.java
@@ -18,6 +18,14 @@ public class MenuAdapter extends FragmentPagerAdapter {
     final int PAGE_COUNT = 5;
     private String tabTitles[] = new String[] { "Ma", "Di", "Wo", "Do", "Vr" };
     MenuFragment fragment;
+    /*MenuFragment fr1 = MenuFragment.newInstance(0);
+    MenuFragment fr2 = MenuFragment.newInstance(1);
+    MenuFragment fr3 = MenuFragment.newInstance(2);
+    MenuFragment fr4 = MenuFragment.newInstance(3);
+    MenuFragment fr5 = MenuFragment.newInstance(4);
+    MenuFragment fr6 = MenuFragment.newInstance(4);*/
+
+
 
     public MenuAdapter(FragmentManager fm) {
         super(fm);
@@ -31,6 +39,14 @@ public class MenuAdapter extends FragmentPagerAdapter {
     @Override
     public Fragment getItem(int position) {
 
+       /* switch (position){
+            case 0:return fr1;
+            case 1:return fr2;
+            case 2:return fr3;
+            case 3:return fr4;
+            case 4:return fr5;
+            default:return fr1;
+        }*/
         fragment = MenuFragment.newInstance(position + 1);
         return fragment;
 
@@ -40,10 +56,6 @@ public class MenuAdapter extends FragmentPagerAdapter {
     public CharSequence getPageTitle(int position) {
         // Generate title based on item position
         return tabTitles[position];
-    }
-
-    public MenuFragment getFragment() {
-        return fragment;
     }
 
 
