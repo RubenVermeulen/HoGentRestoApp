c1dc110ca4780c1c75e8c0c72e146febffe4e00e
Jonas Wallays <jonas.wallays.u8978@student.hogent.be>
2016-12-08 16:57:32 +0100

    Android fragment duplicate in list

--- android/app/src/main/java/resto/android/hogent/be/hogentresto/MenuTabs/MenuFragment.java
+++ android/app/src/main/java/resto/android/hogent/be/hogentresto/MenuTabs/MenuFragment.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -33,6 +34,11 @@ import resto.android.hogent.be.hogentresto.models.Menu;
 // In this case, the fragment displays simple text based on the page
 public class MenuFragment extends Fragment {
 
+    List<Menu> menuMaandag;
+    List<Menu> menuDinsdag;
+    List<Menu> menuWoensdag;
+    List<Menu> menuDonderdag;
+    List<Menu> menuVrijdag;
 
     View view;
     TextView tvTitle;
@@ -48,7 +54,6 @@ public class MenuFragment extends Fragment {
     DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
     Map<String, List<Menu>> menus= new HashMap<String, List<Menu>>();
 
-    List<Menu> menuMaandag, menuDinsdag, menuWoensdag, menuDonderdag, menuVrijdag;
 
     private int mPage;
 
@@ -65,21 +70,25 @@ public class MenuFragment extends Fragment {
         super.onCreate(savedInstanceState);
         mPage = getArguments().getInt(ARG_PAGE);
 
-
     }
 
     // Inflate the fragment layout we defined above for this fragment
     // Set the associated text for the title
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         view = inflater.inflate(R.layout.menu_fragment, container, false);
         tvTitle = (TextView) view.findViewById(R.id.tvTitle);
         datum = (TextView) view.findViewById(R.id.menuDate);
 
         menus = RestaurantActivity.getMenusFromApi();
         LinearLayout fragmentlist = (LinearLayout) view.findViewById(R.id.fragment_list_id);
+        fragmentlist.removeAllViews();
         if(!menus.isEmpty()) {
+            menuMaandag = new ArrayList<>();
+            menuDinsdag = new ArrayList<>();
+            menuWoensdag = new ArrayList<>();
+            menuDonderdag = new ArrayList<>();
+            menuVrijdag = new ArrayList<>();
             menuMaandag = menus.get("maandag");
             menuDinsdag = menus.get("dinsdag");
             menuWoensdag = menus.get("woensdag");
@@ -94,11 +103,9 @@ public class MenuFragment extends Fragment {
                 datum.setText(df.format(menuMaandag.get(0).getAvailableAt()));
                 for (Menu m : menuMaandag) {
                     child = getLayoutInflater(savedInstanceState).inflate(R.layout.menus_list_item, null);
-
                     title = (TextView) child.findViewById(R.id.title);
                     description = (TextView) child.findViewById(R.id.description);
                     price = (TextView) child.findViewById(R.id.price);
-
                     title.setText(m.getTitle());
                     description.setText(m.getProduct().getDescription());
                     price.setText(String.format(Locale.getDefault(), "€ %.2f", m.getPrice()));
@@ -169,7 +176,6 @@ public class MenuFragment extends Fragment {
                     title.setText(m.getTitle());
                     description.setText(m.getProduct().getDescription());
                     price.setText(String.format(Locale.getDefault(), "€ %.2f", m.getPrice()));
-
                     fragmentlist.addView(child);
                 }
                 }
@@ -187,7 +193,6 @@ public class MenuFragment extends Fragment {
                     title = (TextView) child.findViewById(R.id.title);
                     description = (TextView) child.findViewById(R.id.description);
                     price = (TextView) child.findViewById(R.id.price);
-
                     title.setText(m.getTitle());
                     description.setText(m.getProduct().getDescription());
                     price.setText(String.format(Locale.getDefault(), "€ %.2f", m.getPrice()));
