61d0f61dcf7c99bab90f890a4f3375bd9ffc4a9c
Ruben Vermeulen <ruben.vermeulen@hotmail.com>
2017-02-01 00:56:12 +0100

    Removes unused menu button

--- android/app/src/main/java/resto/android/hogent/be/hogentresto/MainActivity.java
+++ android/app/src/main/java/resto/android/hogent/be/hogentresto/MainActivity.java
@@ -113,12 +113,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
     public void refresh(View view) {
         progressBar.setVisibility(View.VISIBLE);
         getRestaurants();
